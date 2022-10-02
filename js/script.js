089ec758f861f6869d49c374fca8fc7b1376c139
Benny Li <59155001+Bennyli1995@users.noreply.github.com>
2022-10-01 22:26:23 -0700

    Added slider to allow user interaction

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
-
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
  * `temperatures array 
@@ -184,6 +176,73 @@ const temperatures = [
   {
   time: "16:57:00",
   temperature: 86439
+  },
+  {time: "16:58:00",
+  temperature: 155054
+  },
+  {
+  time: "16:59:00",
+  temperature: 133501
+  },
+  {
+  time: "17:00:00",
+  temperature: 108082
+  },
+  {
+  time: "17:01:00.00",
+  temperature: 50230
+  },
+  {
+  time: "17:02:00",
+  temperature: 26849
+  },
+  {
+  time: "17:03:00",
+  temperature: 135756
+  },
+  {
+  time: "17:04:00",
+  temperature: 98345
+  },
+  {
+  time: "17:05:00",
+  temperature: 66822
+  },
+  {
+  time: "17:06:00",
+  temperature: 128947
+  },
+  {
+  time: "17:07:00",
+  temperature: 123772
+  },
+  {
+  time: "17:08:00",
+  temperature: 103698
+  },
+  {
+  time: "17:09:00",
+  temperature: 92871
+  },
+  {
+  time: "17:10:00",
+  temperature: 118697
+  },
+  {
+  time: "17:11:00",
+  temperature: 125670
+  },
+  {
+  time: "17:12:00",
+  temperature: 99982
+  },
+  {
+  time: "17:13:00",
+  temperature: 100048
+  },
+  {
+  time: "17:14:00",
+  temperature: 110188
   }]
 
 /***
@@ -199,6 +258,7 @@ const temperatures = [
 
 function getRandomTemp(){
   const randomNumber = Math.floor((Math.random()*temperatures.length))
+  output.innerHTML = randomNumber;
   return temperatures[randomNumber];
 }
 
@@ -212,20 +272,6 @@ function getRandomTemp(){
  * 3. Then use that string to display a random time and temp
  * in the browser.
 ***/
-
-function printTemp(){
-    const tempObject = getRandomTemp();
-
-    let html = `<p class="time"> Time: ${tempObject.time}</p>
-    <p class="temp">Temperature in Kelvin: ${tempObject.temperature}`
-
-    // close off p-tag
-    html += "</p>"
-
-    document.getElementById('temp-box').innerHTML = html
-
-    return html;
-}
 
 /***
  * `randomColor` function
@@ -256,27 +302,72 @@ function randomColor(){
     document.body.style.backgroundImage = "url('150000.jpeg')";
   } else {
     document.body.style.backgroundImage = "url('160000.jpeg')";
-  }}
+  }
+  let html = `<p class="time"> Time: ${tempObject.time}</p>
+  <p class="temp">Temperature in Kelvin: ${tempObject.temperature}`
 
-/***
- * `Timer` function
- * 
- * This is a function that will automatically refresh the quotes every 3s.
- * Function has no returns.
-***/
+  // close off p-tag
+  html += "</p>"
 
-function Timer(){
-  randomColor()
-  printTemp()
-}
+  document.getElementById('temp-box').innerHTML = html
 
-setInterval('Timer()', 3000);
+  return html;}
 
+  function openPopup(){
+    let popup = document.getElementById("popup");
+    popup.classList.add("open-popup");
+  }
+
+  function closePopup(){
+    let popup = document.getElementById("popup");
+    popup.classList.remove("open-popup");
+  }
+  
+  var slider = document.getElementById("myRange");
+  var output = document.getElementById("value");
+  output.innerHTML = slider.value; // Display the default slider value
+
+  // Update the current slider value (each time you drag the slider handle)
+  slider.oninput = function() {
+    output.innerHTML = this.value;
+    const tempObject = temperatures[output.innerHTML]
+      if (tempObject.temperature < 80000) {
+        document.body.style.backgroundImage = "url('70000.jpeg')";
+      } else if (tempObject.temperature < 90000) {
+        document.body.style.backgroundImage = "url('80000.jpeg')";
+      } else if (tempObject.temperature < 100000) {
+        document.body.style.backgroundImage = "url('90000.jpeg')";
+      } else if (tempObject.temperature < 110000) {
+        document.body.style.backgroundImage = "url('100000.jpeg')";
+      } else if (tempObject.temperature < 120000) {
+        document.body.style.backgroundImage = "url('110000.jpeg')";
+      } else if (tempObject.temperature < 130000) {
+        document.body.style.backgroundImage = "url('120000.jpeg')";
+      } else if (tempObject.temperature < 140000) {
+        document.body.style.backgroundImage = "url('130000.jpeg')";
+      } else if (tempObject.temperature < 150000) {
+        document.body.style.backgroundImage = "url('140000.jpeg')";
+      } else if (tempObject.temperature < 160000) {
+        document.body.style.backgroundImage = "url('150000.jpeg')";
+      } else {
+        document.body.style.backgroundImage = "url('160000.jpeg')";
+      }
+      let html = `<p class="time"> Time: ${tempObject.time}</p>
+      <p class="temp">Temperature in Kelvin: ${tempObject.temperature}`
+    
+      // close off p-tag
+      html += "</p>"
+    
+      document.getElementById('temp-box').innerHTML = html
+    
+      return html;}
+
+      
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-document.getElementById('load-temp').addEventListener("click", printTemp, false);
 document.getElementById('load-temp').addEventListener("click", randomColor, false);
+
